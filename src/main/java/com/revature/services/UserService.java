375fb6522c0ec979fdff968075cfecc9a433c04a
DanielOrtyn <33275049+DanielOrtyn@users.noreply.github.com>
2019-06-02 09:10:39 -0400

    Revert "Management team merge"

--- src/main/java/com/revature/services/UserService.java
+++ src/main/java/com/revature/services/UserService.java
@@ -2,8 +2,6 @@ package com.revature.services;
 
 import java.util.List;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.revature.models.User;
@@ -11,7 +9,7 @@ import com.revature.models.User;
 @Service
 public interface UserService {
 	//
-	 public Page<User> findAll(Pageable pageable);
+	// public List<User> findAll();
 	//
 	 public User findOneById(int id);
 	//
@@ -22,11 +20,7 @@ public interface UserService {
 	 public User updateProfile(User u);
 	//
 	 public User findOneByEmail(String email);
-	//
-	 public Page<User> findUserByPartialEmail(String email, Pageable pageable);
-	//
-	 public Page<User> findListByEmail(List<String> emailList, Pageable pageable);
-  
+	 
 	//
 	// public CohortUserListOutputDto saveUsers(UserListInputDto userList, int id)
 	// throws IOException;
